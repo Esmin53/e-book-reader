7486588ee88e8c735a4943ddbb4c8d2fa16954e5
Esmin53 <esmin.tufekcic53@gmail.com>
2024-12-26 14:37:18 +0100

    Add remove from shelf option

--- app/bookshelves/[id].tsx
+++ app/bookshelves/[id].tsx
@@ -6,19 +6,32 @@ import { Link, useLocalSearchParams } from "expo-router"
 import { useSQLiteContext } from "expo-sqlite"
 import { StatusBar } from "expo-status-bar"
 import { useContext, useEffect, useState } from "react"
-import { FlatList, StyleSheet, Text, View } from "react-native"
+import { FlatList, Modal, Pressable, StyleSheet, Text, View } from "react-native"
 
 import AntDesign from '@expo/vector-icons/AntDesign';
 
 const Bookshelf = () => {
         const [books, setBooks] = useState<BookType[]>([])
         const [shelf, setShelf] = useState<string | null>(null)
+        const [isVisible, setIsVisible] = useState<boolean >(false)
 
         const {id} = useLocalSearchParams()
         const db = useSQLiteContext()
         const {theme} = useContext(ThemeContext)
 
         const styles = createStyles(theme)
+
+        const removeBook = async (bookId: number) => {
+            try {
+                await db.runAsync(`
+                    DELETE FROM bookshelves_books WHERE bookshelf_id = ? AND book_id = ?
+                    `, [Number(id), bookId])
+
+                setBooks((prev) => prev.filter((item) => item.id !== bookId))
+            } catch (error) {
+                console.log(error)
+            }
+        }
 
         useEffect(() => {
             const getBooksInBookshelf = async () => {
@@ -73,6 +86,65 @@ const Bookshelf = () => {
                         <AntDesign name="back" size={28} color={theme?.text}/>
                     </Link>
                 </View>
+                <Pressable
+                    onPress={() => setIsVisible(true)}
+                    style={{
+                        paddingHorizontal: 12,
+                        paddingTop: 12
+                    }}
+                >
+                    <Text style={{
+                        color: theme?.text,
+                        fontSize: 16
+                    }}>Remove books from the shelf</Text>
+                </Pressable>
+                <Modal animationType="slide" visible={isVisible} transparent={true}>
+                    <View style={{
+                        flex: 1,
+                        backgroundColor: theme?.secondary
+                    }}>
+                        <View style={{
+                            padding: 12,
+                            display: "flex",
+                            alignItems: "center",
+                            flexDirection: "row",
+                            justifyContent: "space-between",
+                            borderBottomWidth: 1.5,
+                            borderBottomColor: theme?.offGray
+                        }}>
+                            <Text style={{
+                            color: theme?.text,
+                            fontSize: 18
+                            }}>Remove books from the shelf</Text>
+                            <Pressable onPress={() => setIsVisible(false)}>
+                                <AntDesign name="down" size={32} color={theme?.text} />
+                            </Pressable>
+                        </View>
+                        <FlatList 
+                                data={books}
+                                contentContainerStyle={{
+                                padding: 12,
+                                gap: 12
+                            }}
+                            renderItem={({item}) => <View
+                            style={{
+                                display: "flex",
+                                alignItems: "center",
+                                flexDirection: "row",
+                                justifyContent: "space-between",
+                                paddingHorizontal: 2
+                            }}>
+                                <Text style={{
+                                    color: theme?.text,
+                                    fontSize: 18
+                                }}>{item.title}</Text>
+                                <Pressable onPress={() => removeBook(item.id)}>
+                                    <AntDesign name="minus" size={32} color={theme?.text} />
+                                </Pressable>
+                            </View>}
+                            />
+                    </View>
+                </Modal>
                 <FlatList 
                     data={books}
                     contentContainerStyle={{
